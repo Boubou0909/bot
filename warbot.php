a190bcad4c3fd178c1810f881e42aed63139fc10
Balthazar <balthazar0909@gmail.com>
2017-06-13 17:17:02 +0200

    Bot_continu

--- warbot.php
+++ warbot.php
@@ -18,7 +18,7 @@ $botman = BotManFactory::createForRTM([
    'slack_token' => 'xoxb-197653180870-wQ0e9fB26btgngTTQN4K3P6q'
 ], $loop);
 
-$botman->hears('Warhammer begin',function($bot) use (&$etat, &$compteur, &$ligne, &$number, &$fichier){
+$botman->hears('Warhammer begin',function($bot) use (&$etat, &$compteur, &$ligne, &$number, &$fichier, &$compteur){
    $bot->reply('Début du quiz :');
    $fichier = fopen('figurines.txt', 'r');
    rewind($fichier);
@@ -36,12 +36,9 @@ $botman->hears('Warhammer begin',function($bot) use (&$etat, &$compteur, &$ligne
    $bot->reply('(Pour répondre tapez : "Cette figurine est ...".)');
    $ligne = fgets($fichier);
    fclose($fichier);
-   $bot->reply($ligne);
 });
 
-$botman->hears('Cette figurine est {answer}',function($bot, $answer) use (&$etat, &$points, &$ligne, &$number){
-    $bot->reply($ligne);
-    $bot->reply($answer);
+$botman->hears('Cette figurine est {answer}',function($bot, $answer) use (&$etat, &$points, &$ligne, &$number, &$compteur){
     $answer = trim($answer);
     $ligne = trim($ligne);
     if($ligne === $answer){
@@ -49,8 +46,9 @@ $botman->hears('Cette figurine est {answer}',function($bot, $answer) use (&$etat
         $bot->reply('Bonne réponse !');
         $bot->reply('Ton score est de : ' . $points);
     }
-    else if($etat == 2 and $answer == 'end'){
+    else if($etat == 2 and $answer == 'End'){
         $etat = 0;
+        $bot->reply("Ton score est de " . $points . ' sur ' . $compteur);
     }
     else if ($etat != 2){
         $bot->reply("Pour commencer une partie, tapez : \"Warhammer begin\".");
@@ -58,6 +56,31 @@ $botman->hears('Cette figurine est {answer}',function($bot, $answer) use (&$etat
     else if ($etat == 2 and $answer != $ligne){
         $bot->reply("Ce n'est pas la bonne réponse.");
     }
+    $bot->reply('Taper "Ok" pour continuer et "End" pour finir.');
+});
+
+$botman->hears("{continue}",function($bot, $continue) use(&$etat, &$points, &$ligne, &$number, &$compteur) {
+    if ($continue == "Ok") {
+        $fichier = fopen('figurines.txt', 'r');
+        rewind($fichier);
+        $number = rand(0,2);
+        for ($i = 0; $i <= $number; $i++) {
+            $ligne = fgets($fichier);
+            $ligne = fgets($fichier);
+        };
+        $ligne = fgets($fichier);
+        $compteur = $compteur + 1;
+        $bot->reply('Question n°' . $compteur);
+        $bot->reply($ligne);
+        $bot->reply('Qui est-ce ?');
+        $bot->reply('(Pour répondre tapez : "Cette figurine est ...".)');
+        $ligne = fgets($fichier);
+        fclose($fichier);
+    }
+    else if($continue == "End"){
+        $etat = 0;
+        $bot->reply("Ton score est de " . $points . ' sur ' . $compteur);
+    };
 });
 
 $loop->run();
