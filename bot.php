63e8d5a2a93573ca6e3048d476a5e3a4ff551c0e
Balthazar <balthazar0909@gmail.com>
2017-06-13 09:32:11 +0200

    Première_version

---
+++ bot.php
@@ -0,0 +1,78 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Mpociot\BotMan\BotManFactory;
+use React\EventLoop\Factory;
+use Mpociot\BotMan\Messages\Message;
+
+$loop = Factory::create();
+
+$botman = BotManFactory::createForRTM([
+    'slack_token' => 'xoxb-196287387540-AHOhFA8lPkKSHqg1cbR0LthU'
+], $loop);
+
+$botman->hears('hello', function(\Mpociot\BotMan\BotMan $bot) {
+    $bot->typesAndWaits(2);
+    $bot->reply(':wave:');
+});
+
+$botman->hears('call me {name}', function(\Mpociot\BotMan\BotMan $bot, $name){
+    $bot->typesAndWaits(2);
+    $bot->userStorage()->save([
+         'name' => $name
+    ]);
+    $bot->typesAndWaits(2);
+    $bot->reply('Your name is ' . $name);
+});
+
+$botman->hears('I want ([0-9]+)', function (\Mpociot\BotMan\BotMan $bot, $number) {
+    $number = $number + 1;
+    $bot->typesAndWaits(2);
+    $bot->reply('You will get : '. $number);
+});
+
+$botman->receivesImages(function(\Mpociot\BotMan\BotMan $bot, $images) {
+    $bot->reply("Ok");
+});
+
+$botman->hears('keyword', function (\Mpociot\BotMan\BotMan $bot) {
+    $message = Message::create('This is my text');
+    $bot->typesAndWaits(2);
+    $bot->reply($message);
+});
+
+$botman->hears('Hi', function($bot) {
+    $user = $bot->getUser();
+    $bot->typesAndWaits(2);
+    $bot->reply('Hello '.$user->getFirstName().' '.$user->getLastName());
+    $bot->typesAndWaits(1);
+    $bot->reply('your username is: '.$user->getUsername());
+});
+
+$botman->hears('Forget me', function($bot){
+    $bot->typesAndWaits(2);
+    $bot->userStorage()->delete();
+    $bot->reply('I don\'t know anymore who you are.');
+});
+
+$botman->hears('Who am I', function($bot){
+    $user = $bot->userStorage()->get();
+    if ($user->has('name')){
+        $bot->typesAndWaits(2);
+        $bot->reply('Your are ' . $user->get('name'));
+    }
+    else{
+        $bot->typesAndWaits(2);
+        $bot->reply('I don\'t know you yet.');
+    }
+});
+
+$botman->hears('{emoji}',function($bot, $emoji){
+    if ($emoji[0] == ":" and $emoji[strlen($emoji)-1] == ":"){
+        $bot->typesAndWaits(2);
+        $bot->reply('Nice emoji ! :+1:');
+    }
+});
+
+$loop->run();
